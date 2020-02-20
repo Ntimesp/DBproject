d1fe3df8a8a17aa1cdb14a76b05d26c02970e364
Xinpeng Ni <nxp@mail.ustc.edu.cn>
2020-02-20 18:45:38 +0800

    fix a bug

--- app.py
+++ app.py
@@ -1201,7 +1201,7 @@ def hole():
     else:
         sex="女"
 
-    works=workDatabase.query.filter_by(userEmail=current_user.userEmail)
+    works=workDatabase.query.filter(userEmail==current_user.userEmail,workgroup != "delete")
 
     return render_template('hole.html',name=current_user.userNickName,sex=sex,works=works)
 
