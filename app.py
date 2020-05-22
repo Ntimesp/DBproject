4ec8dc34d68d496657918a6513fc80e4c38ca483
SiyuanChen <1079640970@qq.com>
2020-05-22 21:05:47 +0800

    test app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config['SECRET_KEY'] = 'cbYSt76Vck*7^%4d'
 
 conn = pymysql.connect(
     host="localhost",
-    user='root',password="123456",
+    user='root',
     database="lab3",
     charset="utf8")
 
@@ -24,7 +24,7 @@ def index():
 
 @app.route('/campus', methods=['GET', 'POST'])
 def campus():
-    res=[]
+    res = []
     if request.method == 'POST':
         print(request.form)
         cursor = conn.cursor()
@@ -38,6 +38,7 @@ def campus():
             Campus_address=request.form["Campus_address"]
             cursor.execute(sql, [Campus_id, Campus_name, Campus_address])
             flash("操作成功")
+            res = cursor.fetchall()
         elif op=="delete":
             sql="""
             delete from Campus where Campus_id=%s;
@@ -45,6 +46,7 @@ def campus():
             Campus_id=request.form["Campus_id"]
             cursor.execute(sql, Campus_id)
             flash("操作成功")
+            res = cursor.fetchall()
         elif op=="select":
             Campus_id=request.form["Campus_id"]
             Campus_name=request.form["Campus_name"]
@@ -58,7 +60,7 @@ def campus():
 
                 if Campus_name:
                     sql=sql+" and Campus_name="+"\""+Campus_name+"\""
-                
+
                 if Campus_address:
                     sql=sql+" and Campus_address="+"\""+Campus_address+"\""
 
@@ -75,19 +77,104 @@ def campus():
                 sql="update Campus set"
                 if Campus_name:
                     sql=sql+" Campus_name="+"\""+Campus_name+"\","
-                
+
                 if Campus_address:
                     sql=sql+" Campus_address="+"\""+Campus_address+"\""
                 sql=sql+" where Campus_id="+"\""+Campus_id+"\""
                 print(sql)
                 cursor.execute(sql)
                 flash("操作成功")
+                res = cursor.fetchall()
+        cursor.close()
+    else:
+        cursor = conn.cursor()
+        sql = """
+            select * from Campus;
+            """
+        cursor.execute(sql)
+        res = cursor.fetchall()
         cursor.close()
     return render_template('campus.html',campus=res)
 
 @app.route('/major', methods=['GET', 'POST'])
 def major():
+    res = []
+    if request.method == 'POST':
+        print(request.form)
+        cursor = conn.cursor()
+        op = request.form["query"]
+        if op == "insert":
+            sql = """
+                insert into Major (Major_id, Major_name, Major_address, Major_campus_id, Major_leader) 
+                values (%s,%s,%s, %s, %s);
+                """
+            Major_id = request.form["major_id"]
+            Major_name = request.form["major_name"]
+            Major_address = request.form["major_address"]
+            Major_campus_id = request.form["major_campus"]
+            Major_leader = request.form["major_leader"]
+            cursor.execute(sql, [Major_id, Major_name, Major_address, Major_campus_id, Major_leader])
+            flash("操作成功")
+            res = cursor.fetchall()
+        elif op == "delete":
+            sql = """
+                delete from Campus where Campus_id=%s;
+                """
+            Major_id = request.form["major_id"]
+            cursor.execute(sql, Major_id)
+            flash("操作成功")
+            res = cursor.fetchall()
+        elif op == "select":
+            Major_id = request.form["major_id"]
+            Major_name = request.form["major_name"]
+            Major_address = request.form["major_address"]
+            Major_campus_id = request.form["major_campus"]
+            Major_leader = request.form["major_leader"]
+            if not Major_id + Major_name + Major_address + Major_campus_id + Major_leader:
+                sql = "select * from Major"
+            else:
+                sql = "select * from Major where 1=1"
+                if Major_id:
+                    sql = sql + " and Major_id=" + "\"" + Major_id + "\""
+
+                if Major_name:
+                    sql = sql + " and Major_name=" + "\"" + Major_name + "\""
+
+                if Major_address:
+                    sql = sql + " and Major_address=" + "\"" + Major_address + "\""
+                if Major_campus_id:
+                    sql = sql + " and Major_campus_id=" + "\"" + Major_campus_id + "\""
+                if Major_leader:
+                    sql = sql + " and Major_leader=" + "\"" + Major_leader + "\""
+            cursor.execute(sql)
+            flash("操作成功")
+            res = cursor.fetchall()
+        elif op == "update":
+            Major_id = request.form["major_id"]
+            Major_name = request.form["major_name"]
+            Major_address = request.form["major_address"]
+            Major_campus_id = request.form["major_campus"]
+            Major_leader = request.form["major_leader"]
+            if not Major_name + Major_address + Major_campus_id + Major_leader:
+                flash("什么也不做")
+            else:
+                sql = "update Major set"
+                if Major_name:
+                    sql = sql + " Major_name=" + "\"" + Major_name + "\","
+                if Major_address:
+                    sql = sql + " Major_address=" + "\"" + Major_address + "\""
+                if Major_campus_id:
+                    sql = sql + " Major_campus_id=" + "\"" + Major_campus_id + "\""
+                if Major_leader:
+                    sql = sql + " Major_leader=" + "\"" + Major_leader + "\""
+                sql = sql + " where Major_id=" + "\"" + Major_id + "\""
+                print(sql)
+                cursor.execute(sql)
+                flash("操作成功")
+                res = cursor.fetchall()
+        cursor.close()
     return render_template('major.html')
+
 @app.route('/classes', methods=['GET', 'POST'])
 def classes():
     return render_template('class.html')
